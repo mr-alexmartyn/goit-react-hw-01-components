667f14200fb5473411c219e32665571bd2babafc
mr-alexmartyn <111350218+mr-alexmartyn@users.noreply.github.com>
2023-03-17 21:29:39 +0200

    Update ProfileStats.jsx

--- src/components/Profile/ProfileStats.jsx
+++ src/components/Profile/ProfileStats.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import {
   ProfileStats,
   ProfileStatsItem,
@@ -21,3 +22,10 @@ export const Stats = ({ followers, views, likes }) => (
     </ProfileStatsItem>
   </ProfileStats>
 );
+
+
+Stats.propTypes = {
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+};
